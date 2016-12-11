811c065dcdfd34965f800e8efc8fac8f064f2157
Youngchan Je <ochanje210@gmail.com>
2016-12-11 17:57:24 +0900

    Create server.js

---
+++ server.js
@@ -0,0 +1,66 @@
+var express = require('express');
+var serialPort = require('serialport');
+
+var app = express();
+
+var homeData = {
+  data: undefined,
+  temper: undefined,
+  humid: undefined,
+  led: undefined,
+  secure: undefined,
+  dht: undefined,
+};
+
+serialPort.list(function (err, ports) {
+  ports.forEach(function (port) {
+    console.log(port.comName);
+    console.log(port.pnpId);
+    console.log(port.manufacturer);
+  });
+});
+
+var port = new serialPort('/dev/ttyUSB0', {
+  baudRate: 115200,
+});
+
+port.on('open', function () {
+  console.log('opened');
+  port.on('data', function (data) {
+    console.log('Data: ' + data);
+
+    if (isJsonString(data)) {
+      var recvObj = JSON.parse(data);
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        recvObj.date = new Date();
+        homeData = mergeOptions(homeData, recvObj);
+        console.log(homeData);
+      }
+    }
+  });
+});
+
+function isJsonString(str) {
+  try {
+    JSON.parse(str);
+  } catch (e) {
+    return false;
+  }
+
+  return true;
+}
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/' + 'index.html');
+});
+
+app.listen(80, function () {
+  console.log('IoT Server listening on port 80!');
+});
+
+app.get('/rotate/:value', function(req, res) {
+   port.write('rotate:' + req.params.value + '\n');
+   res.end('rotated');
+});
